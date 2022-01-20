291d7781af61142a0486bb303c77e01ad1d9a9de
kchaw1 <heatarin@gmail.com>
2022-01-20 09:10:39 +0900

    first commit

---
+++ src/main/java/com/jpabook/jpashopweb/JpashopWebApplication.java
@@ -0,0 +1,13 @@
+package com.jpabook.jpashopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpabook/jpashopweb/JpashopWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpabook.jpashopweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
