e9d4bcb87ab933347ed0db3b6e2543e3eab0fdc5
kchaw1 <heatarin@gmail.com>
2022-01-26 22:53:38 +0900

    실전편 성능 최적화 first commit

---
+++ src/main/java/com/jpabook/jpashopweb/InitDb.java
@@ -0,0 +1,87 @@
+package com.jpabook.jpashopweb;
+
+import com.jpabook.jpashopweb.domain.*;
+import com.jpabook.jpashopweb.domain.item.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.dbInit1();
+        initService.dbInit2();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final EntityManager em;
+
+        public void dbInit1() {
+            Member member = createMember("userA", "seoul", "nowon", "12345");
+            em.persist(member);
+
+            Book book1 = createBook("JPA1 Book", 10000, 100);
+            em.persist(book1);
+
+            Book book2 = createBook("JPA2 Book", 20000, 100);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, book1.getPrice(), 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, book2.getPrice(), 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+        private Book createBook(String name, int price, int stockQuantity) {
+            Book book1 = new Book();
+            book1.setName(name);
+            book1.setPrice(price);
+            book1.setStockQuantity(stockQuantity);
+            return book1;
+        }
+
+        public void dbInit2() {
+            Member member = createMember("userB", "busan", "dongrae", "11111");
+            em.persist(member);
+
+            Book book1 = createBook("Spring1 Book", 20000, 200);
+            em.persist(book1);
+
+            Book book2 = createBook("Spring2 Book", 40000, 200);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, book1.getPrice(), 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, book2.getPrice(), 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+        private Member createMember(String name, String city, String street, String zipcode) {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address(city, street, zipcode));
+            return member;
+        }
+
+    }
+}
--- src/main/java/com/jpabook/jpashopweb/JpashopWebApplication.java
+++ src/main/java/com/jpabook/jpashopweb/JpashopWebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpashopWebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopWebApplication.class, args);
 	}
